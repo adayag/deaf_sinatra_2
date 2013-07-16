76c4e7fcc8904d0ef44e069e94a4a3e9af67be1a
Apprentice <apprentice@dbc-chicago-009.local>
2013-07-16 16:42:22 -0500

    Start AJAX methods add id's for index.erb

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,14 @@
 get '/' do
+  @grandma = params[:grandma]
   # Look in app/views/index.erb
   erb :index
+end
+
+post '/grandma' do
+  if params[:user_input] == params[:user_input].downcase
+    @grandma = "SPEAK UP YOU LITTLE RASCAL!"
+  else
+    @grandma = "NO NOT SINCE THE WAR!!!"
+  end
+  erb :index :layout => false 
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  // alert("loading doc");
+  $('.talk').submit( function(e) {
+    e.preventDefault();
+    // alert("here");
+    var url = $(this).attr('action');
+    //Using serialize
+    var data = $(this).serialize();
+    var posting = $.post(url, data, function(response){
+      console.log(response);
+     $().html(response);
+    });
+    // posting.done(function(server_response) {
+    //   var content = $(server_response).find( '#response' );
+    //   $( '#response' ).replaceWith( content );
+    // });
+  });
 });
+
+
